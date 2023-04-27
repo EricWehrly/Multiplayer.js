7bf91f1e31df42e28057272c020f86063427ecee
Eric Wehrly <eric.wehrly@gmail.com>
2023-04-26 22:18:54 -0400

    hello world?

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// 'Hello World' nodejs6.10 runtime AWS Lambda function
+exports.handler = (event, context, callback) => {
+    console.log('Hello, world!');
+    callback(null, 'great success');
+}
